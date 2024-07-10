eb33c42b9c708fa768ff8e520fd07ffdf46ee8c3
GustavJones <gustavjones3@gmail.com>
2024-07-10 16:38:45 +0000

    Add image compression and lazy loading

--- scripts/handlers/jpg.py
+++ scripts/handlers/jpg.py
@@ -1,5 +1,10 @@
 import http.server
+from io import BytesIO
+import PIL.Image
+import PIL.ExifTags
 import os
+
+COMPRESSED_SCALE = 0.1
 
 
 def jpg(handlerClass: http.server.BaseHTTPRequestHandler):
@@ -10,16 +15,79 @@ def jpg(handlerClass: http.server.BaseHTTPRequestHandler):
     handlerClass.end_headers()
 
     if handlerClass.path.find("/asset/") != -1:
-        fileName = handlerClass.path.replace("/asset/", "")
+        if handlerClass.path.find("[compress]") != -1:
+            fileName: str
+            fileSize: tuple
+            resizedFile: PIL.Image.Image
+            resizeSize: tuple[int, int]
+            fileExif: PIL.Image.Exif
+            output: BytesIO
 
-        file = open(os.path.join("library", fileName), "rb")
-        handlerClass.wfile.write(file.read())
+            output = BytesIO()
 
-        file.close()
+            fileName = handlerClass.path.replace("[compress]", "")
+            fileName = fileName.replace("/asset/", "")
+
+            file = PIL.Image.open(os.path.join("library", fileName))
+            fileSize = file.size
+
+            fileExif = file.getexif()
+
+            resizeSize = (
+                int(fileSize[0] * COMPRESSED_SCALE),
+                int(fileSize[1] * COMPRESSED_SCALE),
+            )
+
+            resizedFile = file.resize(resizeSize)
+            resizedFile.save(output, "JPEG", exif=fileExif)
+
+            handlerClass.wfile.write(output.getvalue())
+
+            file.close()
+            resizedFile.close()
+            output.close()
+        else:
+            fileName = handlerClass.path.replace("/asset/", "")
+
+            file = open(os.path.join("library", fileName), "rb")
+            handlerClass.wfile.write(file.read())
+
+            file.close()
+
     elif handlerClass.path.find("/images/") != -1:
-        fileName = handlerClass.path
+        if handlerClass.path.find("[compress]") != -1:
+            fileName: str
+            fileSize: tuple
+            resizedFile: PIL.Image.Image
+            resizeSize: tuple[int, int]
+            fileExif: PIL.Image.Exif
+            output: BytesIO
 
-        file = open(os.path.join("web" + fileName), "rb")
-        handlerClass.wfile.write(file.read())
+            output = BytesIO()
 
-        file.close()
+            fileName = handlerClass.path.replace("[compress]", "")
+
+            file = PIL.Image.open("web" + fileName)
+            fileSize = file.size
+
+            fileExif = file.getexif()
+
+            resizeSize = (
+                int(fileSize[0] * COMPRESSED_SCALE),
+                int(fileSize[1] * COMPRESSED_SCALE),
+            )
+
+            resizedFile = file.resize(resizeSize)
+            resizedFile.save(output, "JPEG", exif=fileExif)
+
+            handlerClass.wfile.write(output.getvalue())
+
+            file.close()
+            resizedFile.close()
+            output.close()
+        else:
+            fileName = handlerClass.path
+            file = open(os.path.join("web" + fileName), "rb")
+            handlerClass.wfile.write(file.read())
+
+            file.close()
